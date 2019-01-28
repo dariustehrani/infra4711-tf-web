5f334dbad50ffd9bab1ff4443a0d1cfcac30fa30
Darius Tehrani <darius.tehrani@microsoft.com>
2019-01-28 15:20:36 +0100

    example to use the scalet

---
+++ scaleset.tf
@@ -0,0 +1,23 @@
+module "web_scaleset" {
+# where to load the module from, e.g. local path or git path
+  source                = "./scaleset/"
+  project_name          = "${var.project_name}"
+  resource_group_name   = "${var.resource_group_name}"
+  location              = "${var.location}" 
+  tags                  = "${var.tags}"
+
+# network settings
+subnet_id = "${azurerm_subnet.infra.subnet_id}"
+
+# VM Settings
+vm_sku = "Standard_B2s"
+capacity = "2"
+
+# IAM
+admin_user = "4711"
+path_to_ssh_pubkey = "~/.ssh/4711.pub"
+
+# Application Port to be exposed. 
+application_port = "80"
+
+}
