dc43afd57b7b52cdc666f5e1d7a752efb5e10c96
Darius Tehrani <darius.tehrani@microsoft.com>
2019-01-28 14:57:19 +0100

    custom data + nginx

--- scaleset/custom-data.sh
+++ scaleset/custom-data.sh
@@ -4,5 +4,6 @@ set -e
 exec > >(tee /var/log/custom-data.log|logger -t custom-data -s 2>/dev/console) 2>&1
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-get install ngninx -y
 sudo apt-get autoremove -y
 sudo reboot
