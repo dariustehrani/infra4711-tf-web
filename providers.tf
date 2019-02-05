ab77c50eadb3700a3eff69abdd7cd9ad93e82eae
Darius Tehrani <darius.tehrani@microsoft.com>
2019-02-05 13:27:05 +0000

    adding remote state

--- providers.tf
+++ providers.tf
@@ -1,7 +1,14 @@
 provider "azurerm" {
       version = ">=1.20.0"
 
+}
 
+terraform {
+  backend "azurerm" {
+    storage_account_name = "infra4711"
+    container_name       = "tfstate"
+    key                  = "web4711.terraform.tfstate"
+  }
 }
 
 /*
