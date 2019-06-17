8a322b78fda5ad3ed8e958512a10af6d51e3e10f
Darius Tehrani <darius.tehrani@microsoft.com>
2019-06-17 18:00:36 +0200

    prefix

---
+++ main.tf
@@ -0,0 +1,13 @@
+locals {
+  prefix_snake = "${terraform.workspace}-${var.prefix}"
+  prefix_flat  = "${terraform.workspace}${var.prefix}"
+}
+
+resource "random_id" "random-id" {
+  keepers = {
+    # Generate a new id each time we switch to a new resource group
+    group_name = "${azurerm_resource_group.infra.name}"
+  }
+
+  byte_length = 4
+}
