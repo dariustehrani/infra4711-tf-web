84aa2eafd6220759a3ec80dae1416314f1ed3a4a
Darius Tehrani <darius.tehrani@microsoft.com>
2019-06-17 18:08:16 +0200

    install tf 0.12.1 to agent

--- providers.tf
+++ providers.tf
@@ -17,7 +17,7 @@ provider "null" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name      = "infra4711"
+    resource_group_name  = "infra4711"
     storage_account_name = "infra4711"
     container_name       = "tfstate"
     key                  = "web4711.terraform.tfstate"
